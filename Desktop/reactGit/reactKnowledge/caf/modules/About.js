38bda7b2b49bbd19be49d0680e838bc18cf5a09a
abbott.liu <617489832@qq.com>
2017-10-29 09:47:48 +0800

    project init

--- Desktop/reactGit/reactKnowledge/caf/modules/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default React.createClass({
-    render() {
-        return <div>Ab123out123567</div>
-    }
-})
--- Desktop/reactGit/webKnowledge/webpack03/src/index.js
+++
@@ -1,3 +0,0 @@
-require('./config.js');
-
-console.log(BTY_CONFIG.planet[1].name)
