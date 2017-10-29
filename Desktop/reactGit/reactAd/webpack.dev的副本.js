be551bd55c9d75605bb00e8fd345d600b05cbb1c
abbott.liu <617489832@qq.com>
2017-10-29 09:44:43 +0800

    project init

---
+++ Desktop/reactGit/reactAd/webpack.dev的副本.js
@@ -0,0 +1,20 @@
+/**
+ *  作者：Abbott.liu
+ *  搭建时间：2017年10月5日
+ *  功能：webpack 开发环境
+ */
+
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const config = merge(common, {
+    devtool: 'inline-source-map',
+    devServer: {
+        // contentBase: './static',
+        contentBase: [path.join(__dirname, "./static")], // 本地服务器 加载页面 所在的目录
+        // port:7000,
+        hot: true // 服务器热加载
+    }
+});
+
+module.exports = config;
